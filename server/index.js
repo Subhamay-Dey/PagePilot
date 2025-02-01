a30aedd017a6fcf0108647889e279e4d0ea725ca
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-02 00:16:54 +0530

    Add dotenv support and integrate Google Generative AI for link processing; update .gitignore and package dependencies

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 import * as cheerio from 'cheerio';
 import axios from "axios"
+import dotenv from "dotenv"
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+dotenv.config()
 
 class Scrape {
     static async scareWebpage(url="") {
@@ -25,10 +29,18 @@ class Scrape {
                 console.log(internalLinks);
             }
             if(link == "/") return;
+
+            const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+            const model = genAI.getGenerativeModel({ model: "text-embedding-004"});
+
+            async function run({text}) {
+                const result = await model.embedContent(text);
+                console.log(result.embedding.values);
+            }
+            run({text: link});
         })    
     };
 
 
 }
-return {head: pageHead, body: pageBody, internalLinks, externalLink}
 Scrape.scareWebpage("https://piyushgarg.dev")
