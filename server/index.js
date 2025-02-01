ca54575694099d523fe20d4187596db4dc6b6c99
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-01 23:46:57 +0530

    Enhance web scraping functionality to categorize links as internal or external ✅

--- server/index.js
+++ server/index.js
@@ -9,13 +9,24 @@ class Scrape {
         const pageHead = $('head').html();
         const pageBody = $('body').html();
 
+        const internalLinks = [];
+        const externalLink = [];
+
         console.log({pageHead});
 
         $('a').each((_, el) => {
             const link = $(el).attr('href');
             console.log(link);
+            if(link.startsWith("http") || link.startsWith("https")) {
+                externalLink.push(link);
+                console.log(externalLink);
+            } else {
+                internalLinks.push(link);
+                console.log(internalLinks);
+            }
+            if(link == "/") return;
         })
     }
 }
 
-Scrape.scareWebpage("http://awwwards.com")
+Scrape.scareWebpage("https://piyushgarg.dev")
